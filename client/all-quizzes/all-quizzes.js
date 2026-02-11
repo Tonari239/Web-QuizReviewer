4333ada72bff6e6d60643f448511950a00759836
Vasiliy Ivanov <vasiliy.ivanov.6j@gmail.com>
2026-02-11 23:47:35 +0200

    linked button review quiz
    linked button kinda Pog

--- client/all-quizzes/all-quizzes.js
+++ client/all-quizzes/all-quizzes.js
@@ -28,6 +28,7 @@ else
 
 	const reviewButton = new ButtonCreatorCallback("Рецензирай", (quizId) => {
 		//TODO: redirect to quiz grading page
+		router.redirectTo("../reviews/review-quiz.html?quiz_id=" + quizId);
 	}, "../../Image_resources/Icons/grade.png");
 
 	const buttonsCreatorFunctions = [fillButton, reviewButton];
